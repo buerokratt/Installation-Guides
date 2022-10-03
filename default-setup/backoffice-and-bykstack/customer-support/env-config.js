92f0914b40f5a452ac29bf945f6630041aafc581
varmoh <101868197+varmoh@users.noreply.github.com>
2022-10-03 17:53:51 +0300

    Update env-config.js

--- default-setup/backoffice-and-bykstack/customer-support/env-config.js
+++ default-setup/backoffice-and-bykstack/customer-support/env-config.js
@@ -1,7 +1,7 @@
 window._env_ = {
-  RUUTER_API_URL: 'https://your.address PRIVATE_RUUTER_URL',
-  TIM_API_URL: 'https://your.address TIM_URL',
-  TARA_REDIRECT_URL: 'https://your.address TIM_URL/oauth2/authorization/tara?callback_url=https://your.address CUSTOMER_SERVICE_URL/auth/callback',
-  MONITORING_URL: 'https://your.address MONITORING_URL,' ,
+  RUUTER_API_URL: 'https://PRIVATE_RUUTER_URL',
+  TIM_API_URL: 'https://TIM_URL',
+  TARA_REDIRECT_URL: 'https://TIM_URL/oauth2/authorization/tara?callback_url=https://CUSTOMER_SERVICE_URL/auth/callback',
+  MONITORING_URL: 'https://MONITORING_URL,' ,
   PASSWORD_AUTH_ENABLED: false,
   };
