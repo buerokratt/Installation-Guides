63eec5bd1baabfd99e4ba4c0432f59fb4e6fc7b5
KlviG <78801020+KlviG@users.noreply.github.com>
2022-10-17 14:27:39 +0300

    Update train.sh

--- default-setup/chatbot-and-training/bot_training/train.sh
+++ default-setup/chatbot-and-training/bot_training/train.sh
@@ -9,6 +9,7 @@ rm -rf -v chatbot-train/models >> train.log
 rm -f -v chatbot-train/domain.yml >> train.log
 cp -r -v  chatbot/data chatbot-train >> train.log
 cp -v chatbot/domain.yml chatbot-train >> train.log
+cp -v chatbot/config.yml chatbot-train >> train.log
 mkdir -v chatbot-train/models >> train.log
 docker compose up train-bot
 docker compose up test-bot
