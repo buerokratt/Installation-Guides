916f2e5800dd560c955e007ca90040e986ee9bce
varmoh <101868197+varmoh@users.noreply.github.com>
2022-11-07 13:31:37 +0200

    Add files via upload

---
+++ default-setup/chatbot-and-training/bot_training/bot_training/chatbot_train/readme.md
@@ -0,0 +1,1 @@
+#### Put bot files directly to this folder (put domain.yml, config.yml, actions folder, data folder and other files/folders straight to this folder)
---
+++ default-setup/chatbot-and-training/bot_training/bot_training/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=/your/actual/directory
+
+cd $DIR || exit 1
+echo 'Started training' >> train.log
+rm -rf -v chatbot-train/data >> train.log
+rm -rf -v chatbot-train/models >> train.log
+rm -f -v chatbot-train/domain.yml >> train.log
+cp -r -v  chatbot/data chatbot-train >> train.log
+cp -v chatbot/domain.yml chatbot-train >> train.log
+mkdir -v chatbot-train/models >> train.log
+docker compose up train-bot
+docker compose up test-bot
+docker compose down
